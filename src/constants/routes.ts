91953cb44a88aa25d3c549c0cb1a02b8bf753730
safdar-azeem <safdarazeem00@gmail.com>
2026-02-09 00:32:30 +0500

    Add routes.ts

---
+++ src/constants/routes.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+	AUTH: {
+		LOGIN: '/auth/login',
+		SIGNUP: '/auth/signup',
+		FORGOT_PASSWORD: '/auth/forgot-password',
+		RESET_PASSWORD: '/auth/reset-password',
+		VERIFY_OTP: '/auth/verify-otp',
+	},
+	USER: {
+		DASHBOARD: '/user/dashboard',
+	},
+	HOME: '/',
+}
