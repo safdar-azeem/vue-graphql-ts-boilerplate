99b4b56f9cb746690535df3ee1618bd257ecbdac
safdar-azeem <safdarazeem00@gmail.com>
2026-02-09 00:20:23 +0500

    Add signup.schema.ts

---
+++ src/modules/auth/schema/signup.schema.ts
@@ -0,0 +1,34 @@
+import type { IForm } from 'vlite3'
+
+export const signupSchema: IForm[] = [
+	{
+		name: 'username',
+		label: 'Username',
+		type: 'text',
+		placeholder: 'johndoe',
+		required: true,
+		icon: 'lucide:user',
+	},
+	{
+		name: 'email',
+		label: 'Email Address',
+		type: 'email',
+		placeholder: 'you@example.com',
+		required: true,
+		icon: 'lucide:mail',
+	},
+	{
+		name: 'password',
+		label: 'Password',
+		type: 'password',
+		placeholder: '••••••••',
+		required: true,
+		icon: 'lucide:lock',
+		validation: ({ value }) => {
+			if (!value) return ''
+			if (value.length < 6)
+				return 'Password must be at least 6 characters'
+			return ''
+		},
+	},
+]
