458cee84e23ed76c10d04f5cad113749ba544d97
safdar-azeem <safdarazeem00@gmail.com>
2026-02-09 00:17:00 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { vueApollo } from 'vue-apollo-client/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
 	return {
-		plugins: [vue()],
+		plugins: [
+			vue(),
+			tailwindcss(),
+			vueApollo({
+				schema: `${env.VITE_API_URL}/graphql`,
+			}),
+		],
 		resolve: {
 			alias: {
-				'@': '/src',
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
 			},
 		},
 	}
