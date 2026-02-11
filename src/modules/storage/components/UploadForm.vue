b0eb90839c7e148dd0476879eccd26de380d3319
safdar-azeem <safdarazeem00@gmail.com>
2026-02-12 01:57:15 +0500

    Add UploadForm.vue

---
+++ src/modules/storage/components/UploadForm.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useNotifications, Form } from 'vlite3'
+
+interface Props {
+	folderId: string | null
+	refresh: () => void
+}
+
+const props = defineProps<Props>()
+
+const { showToast } = useNotifications()
+
+const handleUpload = async (_, close) => {
+	try {
+		showToast('Files uploaded successfully', 'success')
+		close?.()
+		props?.refresh?.()
+	} catch (e: any) {
+		showToast(e.message, 'error')
+	}
+}
+</script>
+
+<template>
+	<div class="space-y-4">
+		<Form
+			:schema="[
+				{
+					type: 'fileUploader',
+					name: 'files',
+					label: 'Upload Files',
+					required: true,
+					props: {
+						multiSelect: true,
+					},
+				},
+			]"
+			:folder-id="folderId"
+			@on-submit="handleUpload" />
+	</div>
+</template>
