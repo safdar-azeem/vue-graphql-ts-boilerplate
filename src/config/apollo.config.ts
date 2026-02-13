029e9d1a1ae8977e2e53ebae323a2a1436bc653b
safdar-azeem <safdarazeem00@gmail.com>
2026-02-13 17:57:33 +0500

    Update apollo.config.ts

--- src/config/apollo.config.ts
+++ src/config/apollo.config.ts
@@ -1,4 +1,4 @@
-import { createApollo } from 'vue-apollo-client'
+import { createApollo, getRefreshToken, setToken } from 'vue-apollo-client'
 
 const env = import.meta.env
 
@@ -8,4 +8,50 @@ export const apollo = createApollo({
 	},
 	tokenKey: 'auth_token',
 	allowOffline: true,
+	onLogout: () => {
+		console.log('logout')
+	},
+	refreshToken: async () => {
+		const refreshToken = getRefreshToken()
+		if (!refreshToken) {
+			return null
+		}
+		const response = await fetch(`${env.VITE_API_URL}/graphql`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				query: `
+          mutation RefreshTokens($refreshToken: String!) {
+            refreshTokens(refreshToken: $refreshToken) {
+                token
+                refreshToken
+            }
+          }
+        `,
+				variables: {
+					refreshToken,
+				},
+			}),
+		})
+
+		const { data, errors } = await response.json()
+
+		if (errors || !data?.refreshTokens?.token) {
+			return null
+		}
+
+		console.log(data)
+
+		const { token: newToken, refreshToken: newRefreshToken } =
+			data.refreshTokens
+
+		setToken({
+			token: newToken,
+			refreshToken: newRefreshToken,
+		})
+
+		return newToken
+	},
 })
