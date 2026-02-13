88cb179fdb86d29f86341024718dbb426bfedbe5
safdar-azeem <safdarazeem00@gmail.com>
2026-02-13 19:24:18 +0500

    Update AuthLayout.vue

--- src/modules/auth/layouts/AuthLayout.vue
+++ src/modules/auth/layouts/AuthLayout.vue
@@ -3,16 +3,16 @@ import { Icon } from 'vlite3'
 </script>
 
 <template>
-	<div class="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+	<div class="min-h-screen flex items-center justify-center bg-gray-50">
 		<div class="w-full max-w-md bg-white rounded-xl border overflow-hidden">
 			<div
-				class="bg-muted p-6 border-b border-gray-100 text-center flex items-center justify-center gap-3">
+				class="bg-muted p-4 border-b border-gray-100 text-center flex items-center justify-center gap-3">
 				<div class="flex justify-center">
 					<div
 						class="h-10 w-10 bg-primary text-primary-fg rounded-full flex items-center justify-center shadow-md">
 						<Icon
 							icon="lucide:box"
-							class="h-6 w-6" />
+							class="h-5 w-5" />
 					</div>
 				</div>
 				<h2 class="text-2xl font-bold text-gray-900">Builto App</h2>
