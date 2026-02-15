45e601d661a662cbbdfbc7d82ab3e965ad40c7eb
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:58 +0500

    Update index.ts

--- src/modules/storage/routes/index.ts
+++ src/modules/storage/routes/index.ts
@@ -1,17 +1,16 @@
 import type { RouteRecordRaw } from 'vue-router'
 
 export const storageRoutes: RouteRecordRaw[] = [
-	{
-		path: '/storage',
-		component: () => import('@/layouts/MainLayout.vue'),
-		meta: { requiresAuth: true },
-		children: [
-			{
-				path: '',
-				name: 'Storage',
-				component: () => import('../pages/StoragePage.vue'),
-			},
-		],
-	},
+  {
+    path: '/storage',
+    component: () => import('@/layouts/MainLayout.vue'),
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: '',
+        name: 'Storage',
+        component: () => import('../pages/StoragePage.vue'),
+      },
+    ],
+  },
 ]
-
