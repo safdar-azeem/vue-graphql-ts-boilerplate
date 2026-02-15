0d74b4d3c77e2ec38ae77e9cd7bc67af0631aa95
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:57 +0500

    Update login.schema.ts

--- src/modules/auth/schema/login.schema.ts
+++ src/modules/auth/schema/login.schema.ts
@@ -1,20 +1,20 @@
 import type { IForm } from 'vlite3'
 
 export const loginSchema: IForm[] = [
-	{
-		name: 'email',
-		label: 'Email Address',
-		type: 'email',
-		placeholder: 'you@example.com',
-		required: true,
-		icon: 'lucide:mail',
-	},
-	{
-		name: 'password',
-		label: 'Password',
-		type: 'password',
-		placeholder: '••••••••',
-		required: true,
-		icon: 'lucide:lock',
-	},
+  {
+    name: 'email',
+    label: 'Email Address',
+    type: 'email',
+    placeholder: 'you@example.com',
+    required: true,
+    icon: 'lucide:mail',
+  },
+  {
+    name: 'password',
+    label: 'Password',
+    type: 'password',
+    placeholder: '••••••••',
+    required: true,
+    icon: 'lucide:lock',
+  },
 ]
