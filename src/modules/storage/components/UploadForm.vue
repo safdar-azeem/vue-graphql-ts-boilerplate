361d705aee645e3386ed9ee8fa956c7f78da1486
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:58 +0500

    Update UploadForm.vue

--- src/modules/storage/components/UploadForm.vue
+++ src/modules/storage/components/UploadForm.vue
@@ -2,8 +2,8 @@
 import { useNotifications, Form } from 'vlite3'
 
 interface Props {
-	folderId: string | null
-	refresh: () => void
+  folderId: string | null
+  refresh: () => void
 }
 
 const props = defineProps<Props>()
@@ -11,32 +11,32 @@ const props = defineProps<Props>()
 const { showToast } = useNotifications()
 
 const handleUpload = async (_, close) => {
-	try {
-		console.log('folderId :>> ', props.folderId)
-		showToast('Files uploaded successfully', 'success')
-		close?.()
-		props?.refresh?.()
-	} catch (e: any) {
-		showToast(e.message, 'error')
-	}
+  try {
+    console.log('folderId :>> ', props.folderId)
+    showToast('Files uploaded successfully', 'success')
+    close?.()
+    props?.refresh?.()
+  } catch (e: any) {
+    showToast(e.message, 'error')
+  }
 }
 </script>
 
 <template>
-	<div class="space-y-4">
-		<Form
-			:schema="[
-				{
-					type: 'fileUploader',
-					name: 'files',
-					label: 'Upload Files',
-					required: true,
-					props: {
-						multiSelect: true,
-					},
-				},
-			]"
-			:folder-id="folderId"
-			@on-submit="handleUpload" />
-	</div>
+  <div class="space-y-4">
+    <Form
+      :schema="[
+        {
+          type: 'fileUploader',
+          name: 'files',
+          label: 'Upload Files',
+          required: true,
+          props: {
+            multiSelect: true,
+          },
+        },
+      ]"
+      :folder-id="folderId"
+      @on-submit="handleUpload" />
+  </div>
 </template>
