7ad298fa3ef073a6b06aff9a108f621a35cebb15
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:56 +0500

    Update apollo.config.ts

--- src/config/apollo.config.ts
+++ src/config/apollo.config.ts
@@ -3,26 +3,26 @@ import { createApollo, getRefreshToken, setToken } from 'vue-apollo-client'
 const env = import.meta.env
 
 export const apollo = createApollo({
-	endPoints: {
-		default: `${env.VITE_API_URL}/graphql`,
-	},
-	tokenKey: 'auth_token',
-	allowOffline: true,
-	onLogout: () => {
-		console.log('logout')
-	},
-	refreshToken: async () => {
-		const refreshToken = getRefreshToken()
-		if (!refreshToken) {
-			return null
-		}
-		const response = await fetch(`${env.VITE_API_URL}/graphql`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				query: `
+  endPoints: {
+    default: `${env.VITE_API_URL}/graphql`,
+  },
+  tokenKey: 'auth_token',
+  allowOffline: true,
+  onLogout: () => {
+    console.log('logout')
+  },
+  refreshToken: async () => {
+    const refreshToken = getRefreshToken()
+    if (!refreshToken) {
+      return null
+    }
+    const response = await fetch(`${env.VITE_API_URL}/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
           mutation RefreshTokens($refreshToken: String!) {
             refreshTokens(refreshToken: $refreshToken) {
                 token
@@ -30,28 +30,27 @@ export const apollo = createApollo({
             }
           }
         `,
-				variables: {
-					refreshToken,
-				},
-			}),
-		})
+        variables: {
+          refreshToken,
+        },
+      }),
+    })
 
-		const { data, errors } = await response.json()
+    const { data, errors } = await response.json()
 
-		if (errors || !data?.refreshTokens?.token) {
-			return null
-		}
+    if (errors || !data?.refreshTokens?.token) {
+      return null
+    }
 
-		console.log(data)
+    console.log(data)
 
-		const { token: newToken, refreshToken: newRefreshToken } =
-			data.refreshTokens
+    const { token: newToken, refreshToken: newRefreshToken } = data.refreshTokens
 
-		setToken({
-			token: newToken,
-			refreshToken: newRefreshToken,
-		})
+    setToken({
+      token: newToken,
+      refreshToken: newRefreshToken,
+    })
 
-		return newToken
-	},
+    return newToken
+  },
 })
