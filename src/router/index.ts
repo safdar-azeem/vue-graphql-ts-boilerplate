865decf926a4f59f7c94449da27de56cbc0c7771
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:58 +0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,36 +6,36 @@ import { getToken } from 'vue-apollo-client'
 import { ROUTES } from '@/constants/routes'
 
 const routes = [
-  {
-    path: ROUTES.HOME,
-    redirect: ROUTES.USER.DASHBOARD,
-  },
-  ...authRoutes,
-  ...userRoutes,
-  ...storageRoutes,
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: ROUTES.AUTH.LOGIN,
-  },
+  {
+    path: ROUTES.HOME,
+    redirect: ROUTES.USER.DASHBOARD,
+  },
+  ...authRoutes,
+  ...userRoutes,
+  ...storageRoutes,
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: ROUTES.AUTH.LOGIN,
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+  history: createWebHistory(),
+  routes,
 })
 
 // Navigation Guard
 router.beforeEach((to, _from, next) => {
-  const isAuthenticated = !!getToken()
-  const requiresAuth = to.meta.requiresAuth
+  const isAuthenticated = !!getToken()
+  const requiresAuth = to.meta.requiresAuth
 
-  if (requiresAuth && !isAuthenticated) {
-    next(ROUTES.AUTH.LOGIN)
-  } else if (to.path.startsWith('/auth') && isAuthenticated) {
-    next(ROUTES.USER.DASHBOARD)
-  } else {
-    next()
-  }
+  if (requiresAuth && !isAuthenticated) {
+    next(ROUTES.AUTH.LOGIN)
+  } else if (to.path.startsWith('/auth') && isAuthenticated) {
+    next(ROUTES.USER.DASHBOARD)
+  } else {
+    next()
+  }
 })
 
 export default router
