ff26fb8defee5858c4440869f2708d35ead10198
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:56 +0500

    Update routes.ts

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,14 +1,13 @@
 export const ROUTES = {
-	AUTH: {
-		LOGIN: '/auth/login',
-		SIGNUP: '/auth/signup',
-		FORGOT_PASSWORD: '/auth/forgot-password',
-		RESET_PASSWORD: '/auth/reset-password',
-	},
-	USER: {
-		DASHBOARD: '/user/dashboard',
-	},
-	STORAGE: '/storage',
-	HOME: '/',
+  AUTH: {
+    LOGIN: '/auth/login',
+    SIGNUP: '/auth/signup',
+    FORGOT_PASSWORD: '/auth/forgot-password',
+    RESET_PASSWORD: '/auth/reset-password',
+  },
+  USER: {
+    DASHBOARD: '/user/dashboard',
+  },
+  STORAGE: '/storage',
+  HOME: '/',
 }
-
