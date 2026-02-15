7ce7f8f1a98b6522eec3393f10f81c060f8b28ad
safdar-azeem <safdarazeem00@gmail.com>
2026-02-15 15:14:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { vueApollo } from 'vue-apollo-client/vite'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '')
 
-	return {
-		plugins: [
-			vue(),
-			tailwindcss(),
-			vueApollo({
-				schema: `${env.VITE_API_URL}/graphql`,
-			}),
-		],
-		resolve: {
-			alias: {
-				'@': fileURLToPath(new URL('./src', import.meta.url)),
-			},
-		},
-		server: {
-			port: 4202,
-		},
-	}
+  return {
+    plugins: [
+      vue(),
+      tailwindcss(),
+      vueApollo({
+        schema: `${env.VITE_API_URL}/graphql`,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@/graphql': fileURLToPath(new URL('./src/graphql/generated', import.meta.url)),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+    server: {
+      port: 4202,
+    },
+  }
 })
