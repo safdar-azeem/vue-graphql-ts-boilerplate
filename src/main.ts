693ce3b506183793e4f8338328766e4a928202ce
safdar-azeem <safdarazeem00@gmail.com>
2026-03-11 16:59:38 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import { env } from './utils'
 import { createApp } from 'vue'
+import { i18n } from './config/i18n.config'
 import { apollo } from './config/apollo.config'
-import { createVLite, env, GoogleSignInPlugin } from 'vlite3'
+import { createVLite, GoogleSignInPlugin } from 'vlite3'
 import { uploadHandler } from './services/upload.service'
 
 const app = createApp(App)
@@ -11,15 +13,22 @@ const app = createApp(App)
 const vlite = createVLite({
   services: {
     upload: uploadHandler,
+    t: (key: string, ...args: any[]) => {
+      // @ts-ignore
+      return i18n.global.t(key, ...args)
+    },
   },
 })
 
-app.use(GoogleSignInPlugin, {
-  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-})
+if (env.VITE_GOOGLE_CLIENT_ID) {
+  app.use(GoogleSignInPlugin, {
+    clientId: env.VITE_GOOGLE_CLIENT_ID,
+  })
+}
 
 app.use(router)
 app.use(apollo)
 app.use(vlite)
+app.use(i18n)
 
 app.mount('#app')
