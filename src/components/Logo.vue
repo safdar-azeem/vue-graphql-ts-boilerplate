23055e371b7e7229ec8e774f2e7ae2c44bea94c2
safdar-azeem <safdarazeem00@gmail.com>
2026-03-13 02:09:42 +0500

    Add Logo.vue

---
+++ src/components/Logo.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Icon } from 'vlite3'
+</script>
+
+<template>
+  <div>
+    <div class="flex items-center gap-3 px-2 font-bold text-xl text-gray-900 tracking-tight">
+      <div
+        class="w-8 h-8 bg-primary text-white rounded-lg flex items-center justify-center shadow-sm">
+        <Icon icon="lucide:box" class="w-5 h-5" />
+      </div>
+      AppName
+    </div>
+  </div>
+</template>
